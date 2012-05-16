df3fd99bd83c2d9230acbeaa494e382140f1aceb
Mike Haskel <mike.haskel@gmail.com>
2012-05-16 17:34:57 -0400

    wrote Functor instance

--- Expr.hs
+++ Expr.hs
@@ -4,3 +4,8 @@ data Expr a =
   Leaf !a
   | Apply (Expr a) (Expr a)
   | Lambda String (Expr (Maybe a))
+
+instance Functor Expr where
+  fmap f (Leaf a) = Leaf $ f a
+  fmap f (Apply e e') = Apply (fmap f e) (fmap f e')
+  fmap f (Lambda x e) = Lambda x $ fmap (fmap f) e
