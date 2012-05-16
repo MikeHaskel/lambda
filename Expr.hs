764df9afbecc6b121617ebce372130a46ee5335b
Mike Haskel <mike.haskel@gmail.com>
2012-05-16 19:45:12 -0400

    wrote back-tracking parser

--- Expr.hs
+++ Expr.hs
@@ -1,6 +1,10 @@
 module Expr where
 import qualified Data.Set as Set
 import Data.Set (Set)
+import qualified Data.Map as Map
+import Data.Map (Map)
+import Data.Char
+
 
 data Expr a =
   Leaf !a
@@ -47,3 +51,53 @@ instance Show a => Show (Expr a) where
       let e' = fmap (maybe (Left x') id) e in
       ("fun "++) . (x'++) . (" -> "++) . go ctx' 0 e'
     go ctx d e | d <= 12 = ('(':) . go ctx 0 e . (')':)
+
+instance Read a => Read (Expr a) where
+  -- maintain a map of variable names to leaf values, as well as an
+  -- embedding function to map from primitives to leaf values
+  readsPrec = go Map.empty id where
+    go :: Read b =>
+          Map String a -> (b -> a) -> Int -> String -> [(Expr a, String)]
+    go ctx embd d r =
+      [ (Leaf x, r) |
+        d <= 12,
+        (x, r) <- lexIdent r,
+        x <- case Map.lookup x ctx of
+          Nothing -> []
+          Just x -> [x] ] ++
+      [ (Leaf $ embd a, r) |
+        (a, r) <- readsPrec d r ] ++
+      [ (applyChain (Apply f m) ms, r) |
+        d <= 11,
+        (f, r) <- go ctx embd 12 r,
+        (m, r) <- go ctx embd 12 r,
+        (ms, r) <- goAtoms ctx embd r ] ++
+      [ (Lambda x e, r) |
+        d <= 10,
+        ("fun", r) <- lex r,
+        (x, r) <- lexIdent r,
+        ("->", r) <- lex r,
+        let ctx' = Map.insert x Nothing $ fmap Just ctx,
+        let embd' = Just . embd,
+        (e, r) <- go ctx' embd' 0 r ] ++
+      [ (e, r) |
+        d <= 12,
+        ("(", r) <- lex r,
+        (e, r) <- go ctx embd 0 r,
+        (")", r) <- lex r ]
+    
+    lexIdent r =
+      [ (x, r) |
+        (x, r) <- lex r,
+        length x > 0,
+        isAlpha $ head x,
+        x /= "fun" ]
+    
+    goAtoms ctx embd r = case go ctx embd 12 r of
+      [] -> [([],r)]
+      mReader -> [ (m:ms, r) |
+                   (m, r) <- mReader,
+                   (ms, r) <- goAtoms ctx embd r ]
+    
+    applyChain f [] = f
+    applyChain f (m:ms) = applyChain (Apply f m) ms
