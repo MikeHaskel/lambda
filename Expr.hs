f9d080607a9d139e06d6f3e334a0a4e801a10129
Mike Haskel <mike.haskel@gmail.com>
2012-05-16 17:43:36 -0400

    wrote Monad instance

--- Expr.hs
+++ Expr.hs
@@ -9,3 +9,11 @@ instance Functor Expr where
   fmap f (Leaf a) = Leaf $ f a
   fmap f (Apply e e') = Apply (fmap f e) (fmap f e')
   fmap f (Lambda x e) = Lambda x $ fmap (fmap f) e
+
+instance Monad Expr where
+  return = Leaf
+  Leaf a >>= f = f a
+  Apply e e' >>= f = Apply (e >>= f) (e' >>= f)
+  Lambda x e >>= f = Lambda x (e >>= f') where
+    f' Nothing = Leaf Nothing
+    f' (Just a) = fmap Just $ f a
