abf0a610c7c1c752f481619487955340a3d006eb
Mike Haskel <mike.haskel@gmail.com>
2012-05-16 18:11:24 -0400

    implemented intensional equality

--- Expr.hs
+++ Expr.hs
@@ -23,3 +23,9 @@ whnf (Apply f m) = case whnf f of
   Lambda _ e -> whnf $ e >>= maybe (whnf m) return
   f -> Apply f m
 whnf e = e
+
+instance Eq a => Eq (Expr a) where
+  (==) = \e e' -> go (whnf e) (whnf e') where
+    go (Leaf a) (Leaf a') = a == a'
+    go (Apply f m) (Apply f' m') = go f f' && m == m'
+    go (Lambda _ e) (Lambda _ e') = e == e'
