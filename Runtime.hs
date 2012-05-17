3932ce5c167941246f4cc05443fb4011df269a58
Mike Haskel <mike.haskel@gmail.com>
2012-05-16 20:12:48 -0400

    wrote a simple runtime system

---
+++ Runtime.hs
@@ -0,0 +1,20 @@
+module Runtime where
+import Expr
+
+data Runtime a =
+  RLeaf !a
+  | RFun (Runtime a -> Runtime a)
+
+runbox :: Runtime a -> a
+runbox ~(RLeaf a) = a
+
+rapply :: Runtime a -> Runtime a -> Runtime a
+rapply ~(RFun f) = f
+
+run :: Expr (Runtime a) -> Runtime a
+run (Leaf a) = a
+run (Apply f m) = rapply (run f) (run m)
+run (Lambda _ e) = RFun $ \x -> run $ fmap (maybe x id) e
+
+run_ :: Expr a -> Runtime a
+run_ = run . fmap RLeaf
